b88378cb39f2652c9fc01e3cb4294519b5d4e8ee
Ricardo Tribaldos <579671+tribal2@users.noreply.github.com>
2022-11-13 22:52:06 -0500

    chore: solve exercises up to no. 74

--- exercises/standard_library_types/iterators4.rs
+++ exercises/standard_library_types/iterators4.rs
@@ -13,6 +13,12 @@ pub fn factorial(num: u64) -> u64 {
     // For an extra challenge, don't use:
     // - recursion
     // Execute `rustlings hint iterators4` for hints.
+    (1..=num)
+        .into_iter()
+        .fold(
+            1,
+            |acum, item| acum * item
+        )
 }
 
 #[cfg(test)]
